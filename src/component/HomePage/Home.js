3e8f9f23bb3265a16d9e3f5485a8b3dae8584669
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2020-11-06 20:54:57 +0530

    third update

--- src/component/HomePage/Home.js
+++ src/component/HomePage/Home.js
@@ -66,52 +66,73 @@ const Home = () => {
     "17:30",
     "18:00",
     "18:30"
-  ]
+  ];
+  const VideoConsultations = 45;
+  const AudioConsultations = 31;
+  const InPersonConsultations = 15;
   return (
     <div className="container-fluid mt-5 containersize" style={container}>
       <div className="row">
-        <div
-          className="col-lg-8 col-md-12 py-3 text-center scrollbaredit"
-          style={schedule}>
-          <div className="row m-0 p-0">
-            <Link to="/appointment" className="btn btn-light Appointments">
-              Appointments
-            </Link>
-            <Link to="/appointment" className="btn btn-light Patients">
-              Patients
-            </Link>
-            <Link to="/appointment" className="btn btn-light Messages">
-              Messages
-            </Link>
-            <Link to="/appointment" className="btn btn-light Review">
-              Review
-            </Link>
-            <input
-              className=" form-control mr-sm-2 searchbar"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"/>
+        <div className="col-lg-8 col-md-12">
+          <div
+            className="col-12 m-0 px-0 py-3 text-center scrollbaredit"
+            style={schedule}>
+            <div className="row m-0 p-0">
+              <Link to="/appointment" className="btn btn-light Appointments">
+                Appointments
+              </Link>
+              <Link to="/appointment" className="btn btn-light Patients">
+                Patients
+              </Link>
+              <Link to="/appointment" className="btn btn-light Messages">
+                Messages
+              </Link>
+              <Link to="/appointment" className="btn btn-light Review">
+                Review
+              </Link>
+              <input
+                className=" form-control mr-sm-2 searchbar"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"/>
+            </div>
+            <table className="TableOfDoctorsHomePage">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Reminder</th>
+                  <th>Message</th>
+                </tr>
+                <tr>
+                  <td>{date}</td>
+                  <td>{reminder}</td>
+                  <td>{message}</td>
+                </tr>
+                <tr>
+                  <td>14/01/20</td>
+                  <td>Prescription</td>
+                  <td>Appointment Reminder Appointment ReminderAppointment ReminderAppointment
+                    Reminder</td>
+                </tr>
+              </thead>
+            </table>
           </div>
-          <table className="TableOfDoctorsHomePage">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Reminder</th>
-                <th>Message</th>
-              </tr>
-              <tr>
-                <td>{date}</td>
-                <td>{reminder}</td>
-                <td>{message}</td>
-              </tr>
-              <tr>
-                <td>14/01/20</td>
-                <td>Prescription</td>
-                <td>Appointment Reminder Appointment ReminderAppointment ReminderAppointment
-                  Reminder</td>
-              </tr>
-            </thead>
-          </table>
+          <div className="col-12 text-center">
+            <div className="row p-0 m-0 text-center">
+              <div className="col-4 p-0 m-0">
+                <h1 className="Consultations">{VideoConsultations}</h1>
+                <p className="ConsultationsName">Video Consultations</p>
+              </div>
+              <div className="col-4 p-0 m-0">
+                <h1 className="Consultations">{AudioConsultations}</h1>
+                <p className="ConsultationsName">Audio consultations</p>
+              </div>
+              <div className="col-4 p-0 m-0">
+                <h1 className="Consultations">{InPersonConsultations}</h1>
+                <p className="ConsultationsName">In-person consultations</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-lg-3 col-md-12 py-3 videocallcolumn" style={schedule2}>
           <h3 className="text-center">{todaysDate} {MonthArr[month]}, {DayArr[day]}</h3>
@@ -123,12 +144,12 @@ const Home = () => {
                     <h6 className="m-0">{value}</h6>
                   </div>
                   <div className="col-8 p-0 m-0 marginTop">
-                   { /*<h6 className=" m-0 VideoCallwithPatient">Video Call with Patient ABC</h6>*/}
+                    {/*<h6 className=" m-0 VideoCallwithPatient">Video Call with Patient ABC</h6>*/}
                   </div>
                 </React.Fragment>
               )
             })
-            }
+}
           </div>
           <button className="btn btn-primary setavailbtn">SET AVAILABILITY</button>
           <div className="row m-0 p-0">
